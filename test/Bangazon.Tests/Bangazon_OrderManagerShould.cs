40fc1cdfbf87f3413c68683e5db76f1b8ca9d271
Kristin Caron <krissycaron@gmail.com>
2017-08-07 10:21:37 -0500

    deleted out the commented code from order manager should

--- test/Bangazon.Tests/Bangazon_OrderManagerShould.cs
+++ test/Bangazon.Tests/Bangazon_OrderManagerShould.cs
@@ -3,33 +3,5 @@ using Xunit;
 
 namespace Bangazon.Tests
 {
-    // public class OrderManagerShould
-    // {
-
-    //     private readonly OrderManager _manager;
-
-    //     public OrderManagerShould()
-    //     {
-    //         _manager = new OrderManager();
-    //     }
-
-    //     [Fact]
-    //     public void CreateNewOrder()
-    //     {
-    //         Product kite = new Product();
-    //         _manager.CreateOrder(kite);
-    //     }
-
-    //     [Fact]
-    //     public void ListOrders()
-    //     {
-
-    //     }
-
-    //     [Fact]
-    //     public void AddPaymentTypeToOrder()
-    //     {
-
-    //     }
-    // }
+    
 }
