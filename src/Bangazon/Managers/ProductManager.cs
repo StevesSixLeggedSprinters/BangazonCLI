36aea8a6921bbafb7f4bcc580490cb008a247e1b
KJ Miller <KJ Miller>
2017-08-08 11:29:33 -0500

    Product Comments

--- src/Bangazon/Managers/ProductManager.cs
+++ src/Bangazon/Managers/ProductManager.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using Bangazon.Models;
 
 namespace Bangazon
-{
+{                //The class ProductManager holds all methods that interact with the Product table. Kevin Miller.
     public class ProductManager
-    {                            //cust and prod act like Taco
+    {             // The method AddProduct returns True.
         public bool AddProduct(Customer cust, Product prod)
         {
             return true;
         }
-
+                //The method CreateProduct returns a string that is passed in. 
         public string CreateProduct(string aProduct)
         {
             return aProduct;
