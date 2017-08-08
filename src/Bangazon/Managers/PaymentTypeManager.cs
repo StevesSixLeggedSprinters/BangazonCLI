4b3863aaf86b14c3f88566bb46125c60720f7eee
Kyle Kellums <Kyle@Techs-MacBook-Pro.local>
2017-08-08 11:54:16 -0500

    made spelling corrections PaymentType files

--- src/Bangazon/Managers/PaymentTypeManager.cs
+++ src/Bangazon/Managers/PaymentTypeManager.cs
@@ -4,15 +4,15 @@ using Bangazon.Models;
 
 namespace Bangazon
 {
+    // Comments were authored by Kyle Kellums
     // PaymentTypeManager is a class that holds the method 'AddPaymentType()'
     // This class and method gives the user the ability to add a type of payment (e.g. visa, master card, etc.)
     public class PaymentTypeManager
     {
         // The method 'AddPaymentType' enables the user to add a payment type for a customer. Customer and Payment type are passed in.
-        // PaymenType is a foreign key for oders.
+        // PaymentType is a foreign key for orders.
         // Customer is a foreign key for PaymentType.
 
-        This method 
         public bool AddPaymentType(Customer cst, PaymentType pmt)
         {
             return true;
--- src/Bangazon/Models/PaymentType.cs
+++ src/Bangazon/Models/PaymentType.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Bangazon.Models;
 
 namespace Bangazon
-{
+{   // These comments were authored by Kyle Kellums
     // Class PaymentType is an object that holds the entities below (see lines 10-13)
     // This will allow the user to create the PaymentType object
     public class PaymentType
@@ -13,7 +13,7 @@ namespace Bangazon
         public double AccountNumber { get; set; }
         public int CustomerId { get; set; }
 
-        // PaymentType method stores data inputed by the user.
+        // PaymentType method stores the payment info inputed by the user. 
         // This method hill create the PaymentType, which has a customer, paymentNumber, and payName
         public PaymentType(Customer cust, double paymentNumber, string payName)
         {
