473e2bdec13ea1842d2218abbd27d8ef8bf0d551
Kristin Caron <krissycaron@gmail.com>
2017-08-08 12:08:51 -0500

    started the process of testing orders

--- test/Bangazon.Tests/Bangazon_OrderManagerShould.cs
+++ test/Bangazon.Tests/Bangazon_OrderManagerShould.cs
@@ -1,7 +1,21 @@
 using System;
 using Xunit;
+using Bangazon;
+using Bangazon.Models;
+using System.Collections.Generic;
 
 namespace Bangazon.Tests
 {
-    
+    public class OrderManagerShould
+    {  
+        private OrderManager _orderList;
+
+        [Fact]
+        public void GetCustomerOrdersList()
+        {
+            List<Orders> orderList = _orderList.ListOfCustomerOrders(custId)
+            Assert.IsType<List<Orders>>(custId);
+        }
+
+    }
 }
