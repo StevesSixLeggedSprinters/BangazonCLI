bfa469216f3dbfde4569aa90bb8750379b8d080b
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-08-08 11:08:01 -0500

    added comments to my methods

--- src/Bangazon/Managers/CustomerManager.cs
+++ src/Bangazon/Managers/CustomerManager.cs
@@ -20,12 +20,16 @@ namespace Bangazon
             return _customers;
         }
 
+        //This method was authored by Jordan Dhaenens
+        //This method sets the value stored in _activeCustomer which should be a Customer object. Once set it returns that object as confirmation.
         public Customer SetActiveCustomer(Customer cust)
         {
             _activeCustomer = cust;
             return _activeCustomer;
         }
 
+        //This method was authored by Jordan Dhaenens
+        //This method returns the value stored in _activeCustomer which should be a Customer object
         public Customer GetActiveCustomer()
         {
             return _activeCustomer;
