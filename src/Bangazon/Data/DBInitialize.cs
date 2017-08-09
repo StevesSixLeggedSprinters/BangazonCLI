50f0d12bec18c34383afea71a07910a27bbeb605
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-08-09 17:22:48 -0500

    Added some comments to the class and saving before heading home

--- src/Bangazon/Data/DBInitialize.cs
+++ src/Bangazon/Data/DBInitialize.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.DependencyInjection;
 using Bangazon.Models;
 using System.Threading.Tasks;
@@ -8,14 +8,17 @@ using Bangazon;
 
 namespace Bangazon.Data
 {
-    public static class DBPopulator
+    //This class serves to populate every table in the database with a few items
+    public class DBPopulator
     {
-        public static void Populate(IServiceProvider someServiceProvider)
+        private SqliteConnection _connection;
+        //This method was authored by Jordan Dhaenens
+        //This method serves to perform the actual population of the database
+        public void Populate()
         {
-            using (var context = new BangazonCLIContext(someServiceProvider.GetRequiredService<DBContextOptions<BangazonCLIContext>>()))
-            {
-                
-            }
+            _connection = DBInterface.Connection;
         }
+
+        _connection
     }
 }
