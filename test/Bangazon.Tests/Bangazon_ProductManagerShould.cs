8f3374db90aaa9939413deea58f7bed56fb7455e
Kristin Caron <krissycaron@gmail.com>
2017-08-09 10:37:53 -0500

    completed initial tests for get all products and add product to a customers order

--- test/Bangazon.Tests/Bangazon_ProductManagerShould.cs
+++ test/Bangazon.Tests/Bangazon_ProductManagerShould.cs
@@ -34,11 +34,8 @@ namespace Bangazon.Tests
                 Quantity = 9
             };
 
-
             var prodTest = _something.AddProduct(newCust, newProd);
             Assert.True(prodTest);
-
-
         }
 
         [Fact]
@@ -49,8 +46,30 @@ namespace Bangazon.Tests
             Assert.Equal(thisProd, "Audi R8");
 
         }
+        
+        /*Authored by Kyle Kellums and Krissy Caron 
+        This Test Method needs the orderId(which contains access to the customerId)
+        and takes the productId and adds that to the order for that customer. 
+        This will return a new int of ProductOrderId*/
+        [Fact]
+        public void ShouldAddProductToCustomerOrder()
+        {
+            int orderId = 4;
+            int productId = 7;
 
+            var addedToCart = _something.AddProductToOrder(orderId, productId);
+            Assert.True(addedToCart);
+        }
 
+        /* Authored by Kyle Kellums and Krissy Caron 
+        This test is for the GetAllProducts method. It asserts that what is returned is a List type of all the products in the database.
+        */
+        [Fact]
+        public void ShouldGetAllProductsAvailableInDatabase()
+        {
+            List<Product> listOfAllAvailableProducts = _something.GetAllProducts();
+            Assert.IsType<List<Product>>(listOfAllAvailableProducts);
+        }
 
     }
     
