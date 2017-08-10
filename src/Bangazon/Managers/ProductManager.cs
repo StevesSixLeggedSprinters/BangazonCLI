d348ce06f25a77a3b30d2b75e2e1417f5e5eada5
Kyle Kellums <Kyle@Techs-MacBook-Pro.local>
2017-08-10 12:07:47 -0500

    resolved merge conflicts

--- src/Bangazon/Managers/ProductManager.cs
+++ src/Bangazon/Managers/ProductManager.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using Bangazon.Models;
 
 namespace Bangazon
-{                //The class ProductManager holds all methods that interact with the Product table. Kevin Miller.
+{                
+    //The class ProductManager holds all methods that interact with the Product table. Kevin Miller.
     public class ProductManager
-    {             // The method AddProduct returns True.
+    {             
+        
+        // The method AddProduct returns True.
         public bool AddProduct(Customer cust, Product prod)
         {
             return true;
         }
-                //The method CreateProduct returns a string that is passed in. 
+        
+        //The method CreateProduct returns a string that is passed in. 
         public string CreateProduct(string aProduct)
         {
             return aProduct;
         }
-
     }
 }
