a0fb4d149b1cc820d6a783bda2be03edc67c52b7
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-08-10 15:38:41 -0500

    saving changes before switching branches

--- src/Bangazon/Managers/ProductManager.cs
+++ src/Bangazon/Managers/ProductManager.cs
@@ -2,27 +2,35 @@ using System;
 using System.Collections.Generic;
 using Bangazon.Models;
 
+
 namespace Bangazon
 {
     public class ProductManager
     {   //cust and prod act like Taco
 
         private List<Product> _productList = new List<Product>();
+        private DatabaseInterface _db;
+
+        public ProductManager
 
         //This method was authored by Jordan Dhaenens
         //Is creates a product line item in the database and returns the id response from the database
         public int AddProduct(double price, string title, string descript, DateTime prodAdded, int quant, int custID, int prodTypeId)
         {
-            Product prod = new Product()
-            {
-                Price = price,
-                Title = title,
-                Description = descript,
-                DateProductAdded = prodAdded,
-                Quantity = quant,
-                CustomerId = custID, 
-                ProductTypeId = prodTypeId
-            };
+           
+
+
+
+            // Product prod = new Product()
+            // {
+            //     Price = price,
+            //     Title = title,
+            //     Description = descript,
+            //     DateProductAdded = prodAdded,
+            //     Quantity = quant,
+            //     CustomerId = custID, 
+            //     ProductTypeId = prodTypeId
+            // };
             _productList.Add(prod);
             return 1;
         }
--- src/Bangazon/Program.cs
+++ src/Bangazon/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using Bangazon.Models;
+
 
 namespace Bangazon
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             // Seed the database if none exists
             // var db = new DatabaseInitializer();
             // db.VerifyDataExists();
 
+            ProductTypeManager prodTypeManager = new ProductTypeManager();
+            ProductManager prodManager = new ProductManager();
+            
             // Present the main menu
             Console.WriteLine ("*************************************************");
             Console.WriteLine ("Welcome to Bangazon! Command Line Ordering System");
@@ -43,6 +50,36 @@ namespace Bangazon
                 Console.Write ("> ");
                 string phoneNumber = Console.ReadLine();
                 // CustomerManager manager = new CustomerManager();
+            }
+            //This choice is authored by Jordan Dhaenens
+            if (choice == someNumberFromMainMenu)
+            {
+                Console.Clear();
+                Console.WriteLine ("Choose ProductType");
+                //display product types and accept input from user to select a type
+                List<ProductType> types = prodTypeManager.GetProductTypes();
+                foreach (ProductType type in types)
+                {
+                    Console.WriteLine($"{type.ProductTypeId}. {type.Name}");
+                }
+
+                Console.WriteLine(">");
+                int prodTypeID = int.Parse(Console.ReadLine());
+
+                //capture product info
+                Console.Clear();
+                Console.WriteLine("Enter price");
+                double price = double.Parse(Console.ReadLine()); 
+                Console.WriteLine("Enter title");
+                string title = Console.ReadLine();
+                Console.WriteLine("Enter description"); 
+                string description = Console.ReadLine();
+                DateTime date = DateTime.Now;
+                Console.WriteLine("Enter quantity to list");
+                int quantity = int.Parse(Console.ReadLine());
+                //get activeCustomer id
+                int custId = manager.ActiveCustomer.CustomerId;
+                prodManager.AddProduct(price, title, description, date, quantity, custId, prodTypeID);
             }
         }
     }
