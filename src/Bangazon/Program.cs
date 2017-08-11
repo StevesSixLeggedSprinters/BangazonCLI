26f41ddca9bcfad42cf515a0bafe830a3a9669d4
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-08-11 13:53:38 -0500

    everything for ticket for is complete. Waiting on ticket 1 and 2 for integration

--- src/Bangazon/Program.cs
+++ src/Bangazon/Program.cs
@@ -29,7 +29,7 @@ namespace Bangazon
             
             //Instance of customer manager. 
             CustomerManager manager = new CustomerManager(db);
-            ProductTypeManager prodTypeManager = new ProductTypeManager();
+            ProductTypeManager prodTypeManager = new ProductTypeManager(db);
             ProductManager prodManager = new ProductManager(db);
   
             // Present the main menu
@@ -37,6 +37,7 @@ namespace Bangazon
             Console.WriteLine ("Welcome to Bangazon! Command Line Ordering System");
             Console.WriteLine ("*************************************************");
             Console.WriteLine ("1. Create a customer account");
+            Console.WriteLine ("3. Add product to customer profile");
 			Console.Write ("> ");
 
 			// Read in the user's choice
@@ -64,8 +65,13 @@ namespace Bangazon
                 manager.CreateCustomer(firstName, lastName, email, phoneNumber, DateTime.Now);
             }
             //This choice is authored by Jordan Dhaenens
-            if (choice == someNumberFromMainMenu)
+            if (choice == 3)
             {
+                //Check that there is a customer selected as active
+                if (CustomerManager.ActiveCustomerId == 0)
+                {
+                    choice = 2;
+                }
                 Console.Clear();
                 Console.WriteLine ("Choose ProductType");
                 //display product types and accept input from user to select a type
@@ -74,10 +80,9 @@ namespace Bangazon
                 {
                     Console.WriteLine($"{type.ProductTypeId}. {type.Name}");
                 }
-
+                //Capture ProductTypeId
                 Console.WriteLine(">");
                 int prodTypeID = int.Parse(Console.ReadLine());
-
                 //capture product info
                 Console.Clear();
                 Console.WriteLine("Enter price");
@@ -91,6 +96,7 @@ namespace Bangazon
                 int quantity = int.Parse(Console.ReadLine());
                 //get activeCustomer id
                 int custId = CustomerManager.ActiveCustomerId;
+
                 prodManager.AddProduct(price, title, description, date, quantity, custId, prodTypeID);
             }
         }
